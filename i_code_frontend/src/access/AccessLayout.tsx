c0d5eeadbb700445bbe02530441d4dccf112eae5
Alan Xu <alanxu@Mac.35G>
2024-11-28 23:56:28 +1100

    sync

---
+++ i_code_frontend/src/access/AccessLayout.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { usePathname } from "next/navigation";
+import { useSelector } from 'react-redux';
+
+
+
+
+const AccessLayout = () => {
+  return (
+    <div>AccessLayout</div>
+  )
+}
+
+export default AccessLayout
